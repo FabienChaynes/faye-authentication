c96f49ccbd6f573339a3da9cb1b957a695a9cd91
Adrien Siami <adrien.siami@dimelo.com>
2014-05-22 12:34:47 +0200

    FIx bad use of symbol instead of strings for http client

--- lib/faye/authentication/http_client.rb
+++ lib/faye/authentication/http_client.rb
@@ -7,8 +7,8 @@ module Faye
       def self.publish(url, channel, data, key)
         uri = URI(url)
         req = Net::HTTP::Post.new(url)
-        message = {channel: channel, data: data, clientId: 'http'}
-        message[:signature] = Faye::Authentication.sign(message, key)
+        message = {'channel' => channel, 'data' => data, 'clientId' => 'http'}
+        message['signature'] = Faye::Authentication.sign(message, key)
         req.set_form_data(message: JSON.dump(message))
         Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') { |http| http.request(req) }
       end
--- spec/lib/faye/authentication/http_client_spec.rb
+++ spec/lib/faye/authentication/http_client_spec.rb
@@ -6,8 +6,8 @@ describe Faye::Authentication::HTTPClient do
   describe '.publish' do
 
     it 'should publish a HTTP request with correct params' do
-      message = {channel: '/foo/bar', data: 'hello', clientId: 'http'}
-      message[:signature] = Faye::Authentication.sign(message, 'my private key')
+      message = {'channel' => '/foo/bar', 'data' => 'hello', 'clientId' => 'http'}
+      message['signature'] = Faye::Authentication.sign(message, 'my private key')
       request = stub_request(:post, "http://www.example.com").with(:body => {:message => JSON.dump(message)}).to_return(:status => 200, :body => "", :headers => {})
       Faye::Authentication::HTTPClient.publish('http://www.example.com', '/foo/bar', "hello", 'my private key')
       expect(request).to have_been_made
