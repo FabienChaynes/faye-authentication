fbb924782e2279f61257a79508dc1d1d13fa4ab1
Evgeniy Serykh <evserykh@users.noreply.github.com>
2015-04-09 16:33:54 +0500

    uri.request_uri instead of uri.path to avoid blank

--- lib/faye/authentication/http_client.rb
+++ lib/faye/authentication/http_client.rb
@@ -6,7 +6,7 @@ module Faye
 
       def self.publish(url, channel, data, key)
         uri = URI(url)
-        req = Net::HTTP::Post.new(uri.path)
+        req = Net::HTTP::Post.new(uri.request_uri)
         message = {'channel' => channel, 'clientId' => 'http'}
         message['signature'] = Faye::Authentication.sign(message, key)
         message['data'] = data
