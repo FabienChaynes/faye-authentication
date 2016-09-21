0c7447a3f82255dc476920cfb54d21be548d222c
Vincent Boisard <vincent.boisard@dimelo.com>
2016-09-21 16:21:30 +0200

    better: extract request building from publish method to allow reuse in a connection pool

--- lib/faye/authentication/http_client.rb
+++ lib/faye/authentication/http_client.rb
@@ -6,12 +6,17 @@ module Faye
 
       def self.publish(url, channel, data, key)
         uri = URI(url)
-        req = Net::HTTP::Post.new(uri.request_uri)
+        req = prepare_request(uri.request_uri, channel, data, key)
+        Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') { |http| http.request(req) }
+      end
+
+      def self.prepare_request(uri, channel, data, key)
+        req = Net::HTTP::Post.new(uri)
         message = {'channel' => channel, 'clientId' => 'http'}
         message['signature'] = Faye::Authentication.sign(message, key)
         message['data'] = data
         req.set_form_data(message: JSON.dump(message))
-        Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') { |http| http.request(req) }
+        req
       end
 
     end
